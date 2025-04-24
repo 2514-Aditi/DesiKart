800b5eb7432b7d86f6bf08a42e3e4f54db7adc3f
Aditi Nadiger <aditinadiger14@gmail.com>
2025-04-24 20:19:30 +0530

    Add files via upload

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose');
 
+const productSchema = new mongoose.Schema({
+  sellerName: String,
+  productName: String,
+  category: String,
+  description: String,
+  price: Number,
+  image: String,
+  contact: String,
+  address: String
+});
+
+module.exports = mongoose.model('Product', productSchema);
