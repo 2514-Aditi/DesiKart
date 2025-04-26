809c322dad6e105c8a30391923fab718a8827055
Aditi Nadiger <aditinadiger14@gmail.com>
2025-04-26 15:02:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,17 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+
+// Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' })); // Adjust payload size limit
+app.use(express.urlencoded({ extended: true, limit: '50mb' })); // For URL-encoded payloads
 
 // Import Routes
 const productRoutes = require('./routes/productRoutes');
 app.use('/api/products', productRoutes);
-
+const paymentRoutes = require('./routes/paymentRoutes'); // Adjust path if necessary
+app.use('/api/payments', paymentRoutes);
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
